d2579201ae02027f81a74b5760441730b623a8b9
nosh1723 <0702201088abc@gmail.com>
2024-03-16 00:34:43 +0700

    update api

--- src/services/index.js
+++ src/services/index.js
@@ -2,18 +2,21 @@ import axios from "axios";
 
 const registerService = async (data) => {
   try {
-    return await axios.post("http://localhost:6969/api/register", {
-      name: data.name,
-      email: data.email,
-      password: data.password,
-    });
+    return await axios.post(
+      "https://dark-gray-fly-tam.cyclic.app/api/register",
+      {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      }
+    );
   } catch (error) {
     console.log(error);
   }
 };
 const loginService = async (data) => {
   try {
-    return await axios.post("http://localhost:6969/api/login", {
+    return await axios.post("https://dark-gray-fly-tam.cyclic.app/api/login", {
       email: data.email,
       password: data.password,
     });
@@ -25,19 +28,24 @@ const loginService = async (data) => {
 //manga service
 const createManga = async (data) => {
   try {
-    return await axios.post("http://localhost:6969/api/create-manga/", {
-      name: data.name,
-      updatedAt: data.updatedAt,
-      lastChapter: data.lastChapter,
-      imageUrl: data.imageUrl,
-    });
+    return await axios.post(
+      "https://dark-gray-fly-tam.cyclic.app/api/create-manga",
+      {
+        name: data.name,
+        updatedAt: data.updatedAt,
+        lastChapter: data.lastChapter,
+        imageUrl: data.imageUrl,
+      }
+    );
   } catch (error) {
     console.log(error);
   }
 };
 const getAllManga = async () => {
   try {
-    const data = await axios.get("http://localhost:6969/api/all-manga");
+    const data = await axios.get(
+      "https://dark-gray-fly-tam.cyclic.app/api/all-manga"
+    );
     return data.data.DT;
   } catch (error) {
     console.log(error);
@@ -45,26 +53,33 @@ const getAllManga = async () => {
 };
 const getManga = async (id) => {
   try {
-    return (await axios.get(`http://localhost:6969/api/manga/${id}`)).data.DT;
+    return (
+      await axios.get(`https://dark-gray-fly-tam.cyclic.app/api/manga/${id}`)
+    ).data.DT;
   } catch (error) {
     console.log(error);
   }
 };
 const updateManga = async (id, data) => {
   try {
-    return await axios.put(`http://localhost:6969/api/update-manga/${id}`, {
-      name: data.name,
-      updatedAt: data.updatedAt,
-      lastChapter: data.lastChapter,
-      imageUrl: data.imageUrl,
-    });
+    return await axios.put(
+      `https://dark-gray-fly-tam.cyclic.app/api/update-manga/${id}`,
+      {
+        name: data.name,
+        updatedAt: data.updatedAt,
+        lastChapter: data.lastChapter,
+        imageUrl: data.imageUrl,
+      }
+    );
   } catch (error) {
     console.log(error);
   }
 };
 const deleteManga = async (id) => {
   try {
-    await axios.delete(`http://localhost:6969/api/delete-manga/${id}`);
+    await axios.delete(
+      `https://dark-gray-fly-tam.cyclic.app/api/delete-manga/${id}`
+    );
   } catch (error) {
     console.log(error);
   }
